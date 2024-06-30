093cf957f197abafde888f751b1f2ef1af72072f
nekomi2 <dore123222@gmail.com>
2024-06-30 12:12:51 +0700

    add link input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
-    let count = 0;
+  let count = 0;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mainbox">
+  <h1>Titanium</h1>
+  <input class="url-input" type="text" placeholder="Insert video link" />
+</div>
+
+<style>
+  .mainbox {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .url-input {
+    background: none;
+    border: none;
+    border-bottom: 1px solid gray;
+    outline: none;
+    font-size: 0.8rem;
+    height: 2rem;
+  }
+</style>
